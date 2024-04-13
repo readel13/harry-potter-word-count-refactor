92eb8d881f39cf795bb77a1838912aab276580a7
readel13 <vtecready1@gmail.com>
2024-04-13 21:49:41 +0300

    optimized imports

--- src/edu/pro/refactored/MainRefactored.java
+++ src/edu/pro/refactored/MainRefactored.java
@@ -1,26 +1,7 @@
 package edu.pro.refactored;
 
 import edu.pro.refactored.utils.AnalyticsUtil;
-import edu.pro.refactored.utils.FileUtil;
 import edu.pro.refactored.utils.MeasureTimeUtil;
-import edu.pro.refactored.utils.WordUtil;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.AbstractMap;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class MainRefactored {
 
